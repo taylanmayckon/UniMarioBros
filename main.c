8aa1e6955ebe0b2721d696b2aacf2d9e9e97c7f1
taylanmayckon <eusoutaylann@gmail.com>
2025-06-03 02:06:51 -0300

    Fix: Importação de scene.h

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include "raylib.h"
 #include "mario.h"
+#include "scene.h"
 
 // Inicializando Mario_t
 Mario_t Mario;
-
-// Variavel de tempo para testes
-float timer = 0.0f;
 
 int main(void){
     InitWindow(800, 600, "UniMarioBros");
@@ -16,27 +14,8 @@ int main(void){
     GameScene scene = CreateGameScene("assets/textures/background1.png"); // Função da biblioteca Scene para configurar o cenário
 
     InitMario(&Mario); // Inicializando as structs do Mario com valores
-    Mario.actualState = ACTION_WALKING;
     
     while (!WindowShouldClose()) {
-        // Estrutura basica pra testar troca de animação
-        if(timer<=5.0f){
-           timer += GetFrameTime();
-           Mario.powerUpState = STATE_SMALL;
-        }
-        else if(timer<=10.0f){
-            timer += GetFrameTime();
-            Mario.powerUpState = STATE_SUPER;
-            //Mario.facingRight = !Mario.facingRight;
-        }
-        else if(timer<=15.0f){
-            timer += GetFrameTime();
-            Mario.powerUpState = STATE_FIRE;
-        }
-        else{
-            timer = 0.0f;
-        }
-
         BeginDrawing();
         ClearBackground(RAYWHITE);
         
